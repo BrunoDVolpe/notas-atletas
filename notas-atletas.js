6f2a987c8775d3c7e3877ad9193895da58cefd72
Bruno Volpe <bdva16@hotmail.com>
2022-12-09 22:26:19 -0300

    upload notas-atletas.js, README and preview
    first commit.

---
+++ notas-atletas.js
@@ -0,0 +1,33 @@
+﻿function competicaoAlunos(atletas){
+  for(let i = 0; i < atletas.length; i++) {
+    let notasValidas = atletas[i].notas.sort().slice(1,4)
+    console.log(`Atleta: ${atletas[i].nome}
+Notas Obtidas: ${atletas[i].notas}
+Média Válida: ${notasValidas.reduce(function(total, atual){
+  return total + atual
+}, 0) / notasValidas.length}
+`)
+    }
+}
+
+
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+
+competicaoAlunos(atletas)
